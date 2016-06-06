4efd77d56a4cdb5bdd16a8661634abc263cfc5ae
appfrancois <franswa@Francois-Duchemins-iMac-2.local>
2016-06-06 20:50:28 +0200

    Multiple components

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HeroDetailComponent } from './hero-detail.component';
 
 export class Hero {
   id: number;
@@ -8,23 +9,16 @@ export class Hero {
 @Component({
   selector: 'my-app',
   template:`
-    <div *ngIf="selectedHero">
-      <h2>{{selectedHero.name}} details!</h2>
-      <div><label>id: </label>{{selectedHero.id}}</div>
-      <div>
-          <label>name: </label>
-          <input [(ngModel)]="selectedHero.name" placeholder="name"/>
-      </div>
-    </div>
-    
+    <h1>{{title}}</h1>
     <h2>My Heroes</h2>
     <ul class="heroes">
       <li *ngFor="let hero of heroes"
-        [class.selected]="hero === selectedHero" 
+        [class.selected]="hero === selectedHero"
         (click)="onSelect(hero)">
         <span class="badge">{{hero.id}}</span> {{hero.name}}
       </li>
     </ul>
+    <my-hero-detail [hero]="selectedHero"></my-hero-detail>
     `,
   styles:[`
     .selected {
@@ -74,7 +68,8 @@ export class Hero {
       margin-right: .8em;
       border-radius: 4px 0 0 4px;
     }
-  `]
+  `],
+  directives: [HeroDetailComponent]
 })
 export class AppComponent {
   title = 'Tour of Heroes';
